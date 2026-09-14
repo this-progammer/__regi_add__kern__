9dc4d69cd71aee9a8a750100bb65a6280b8a6aa6
Aether <140750964+this-progammer@users.noreply.github.com>
2026-09-14 13:49:42 -0500

    Update registers.c

--- registers.c
+++ registers.c
@@ -4,6 +4,23 @@
 
 #define _ASCII 0x011
 
+/*register value*/
+#define _MOV 0x1
+
+struct reg_tbl {
+    int mov;
+    int vm; /*video mode*/
+};
+
+void mov_vm_value( struct reg_tbl* r ) {
+    char mov_ascii[3] = "mov";
+    char vm_ascii[2] = "vm";
+        for( int i = 0 ; i < r->mov; i++ ) {
+                while( r->mov != 0 ) {
+                    _ASCII << mov_ascii;
+                }
+        }
+}
 
 
 /*main()*/
