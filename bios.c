8c8bc02dcce7d8ed8f602f19cc7346b7f9fa7270
Aether <140750964+this-progammer@users.noreply.github.com>
2026-09-15 10:35:46 -0500

    Update bios.c

--- bios.c
+++ bios.c
@@ -1,12 +1,17 @@
 /*bios.c*/
-
 #define BIOS_C
 
 /*dword*/
 #define dword unsigned int
 
+/*boot sector info*/
 static struct bios_k {
     dword bs_org = 0x7C00; /*boot sector address*/
     int bs_bits = 16; /*real mode*/
-    dword end_org = 0x55AA;/*end of 512 kb boot sector*/
+    dword end_org = 0x55AA;/*end of 512 KB boot sector*/
 };
+
+/*get_boot_sector_org()*/
+dword get_boot_sector_org( struct bios_k* k ) {
+    return k->bs_org;
+}
