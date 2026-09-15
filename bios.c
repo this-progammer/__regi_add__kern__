940b871461261088323aa18ab86cac03ff35149d
Aether <140750964+this-progammer@users.noreply.github.com>
2026-09-15 10:31:00 -0500

    Update bios.c

--- bios.c
+++ bios.c
@@ -2,9 +2,11 @@
 
 #define BIOS_C
 
+/*dword*/
 #define dword unsigned int
 
 static struct bios_k {
     dword bs_org = 0x7C00; /*boot sector address*/
     int bs_bits = 16; /*real mode*/
+    dword end_org = 0x55AA;
 };
