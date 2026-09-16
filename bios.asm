2cf1491ed9da6dcb5713b52266954acabe478961
Aether <140750964+this-progammer@users.noreply.github.com>
2026-09-16 09:24:54 -0500

    Update bios.asm

--- bios.asm
+++ bios.asm
@@ -1,3 +1,6 @@
 ;  *bios.asm
 
-section .000000  
+section .000000  section .data
+section .000001  __bios_org : DWORD 0x10
+section .000002   BIOS_K:
+section .000003      mov ebx DWORD PTR, __bios_org
